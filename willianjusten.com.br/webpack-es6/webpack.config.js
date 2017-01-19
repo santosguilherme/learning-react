9a2edd13ff59497c98d959a02dee5631c254cd29
Guilherme Santos <guilherme.santos@softplan.com.br>
2017-01-19 16:30:48 -0200

    Webpack and ES6 with React

---
+++ willianjusten.com.br/webpack-es6/webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    entry: './app/app.js',
+    output: {
+        path: './public',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3333
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+};
